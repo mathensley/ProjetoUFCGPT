8c51fa7612ad80656a1ad1165d81d4cfc7d6dff8
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2024-12-19 11:27:36 -0300

    refactor: Refatorando arquivo de localizacoes

--- src/agents/agent_class.py
+++ src/agents/agent_class.py
@@ -4,9 +4,17 @@ from typing import TypedDict, Annotated, Sequence, Literal
 from langchain_core.messages import BaseMessage
 from pydantic import BaseModel
 
-AGENTS = ["Agente_Cursos_Eureca", "Agente_Disciplinas_Turmas_Eureca", "Agente_Campus_Eureca", 
-          "Agente_Setor_Professor_Estagio_Eureca", "Agente_Resolucao", "Agente_Matriculas", 
-          "Agente_Localizacao", "Agente_Comunicados_Oficiais", "Agente_Sumarizador"]
+AGENTS = [
+    "Agente_Cursos_Eureca", 
+    "Agente_Disciplinas_Turmas_Eureca", 
+    "Agente_Campus_Eureca", 
+    "Agente_Setor_Professor_Estagio_Eureca", 
+    "Agente_Resolucao", 
+    "Agente_Matriculas", 
+    "Agente_Localizacao", 
+    "Agente_Comunicados_Oficiais", 
+    "Agente_Sumarizador"
+]
 OPTIONS = ("FINALIZAR",) + tuple(AGENTS)
 
 class RouteResponse(BaseModel):
